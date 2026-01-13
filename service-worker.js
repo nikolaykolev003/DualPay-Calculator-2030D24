c4762445d83e25a572af099612b2c4162a57b8eb
nikolaykolev003 <nikolaykolev003@gmail.com>
2026-01-13 23:08:03 +0200

    Update service-worker.js
    дд

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,22 @@
-const CACHE_NAME = "dualpay-cache-v1";
-
+const CACHE_NAME = 'dualpay-cache-v1';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
